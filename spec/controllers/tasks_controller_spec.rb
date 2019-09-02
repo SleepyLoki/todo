45bfc5a324cef26cc9b6977c2ed674f3594dcd7d
Enrique Cerna <ecerna96@gmail.com>
2019-09-02 16:50:48 +0000

    refactored code -lesson 13

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
-    it "should list the tasks in the dabase" do
+    it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
       task1.update_attributes(title: "Something else")
